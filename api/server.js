f5fc371990088d0cad8ff1688546eb80458a61be
freshworkslab <164862658+freshworkslab@users.noreply.github.com>
2024-09-04 19:28:27 +0530

    Update server.js

--- api/server.js
+++ api/server.js
@@ -4,8 +4,8 @@ const axios = require('axios');
 const app = express();
 app.use(express.json());
 
-const FRESHSALES_DOMAIN = 'your-domain.freshsales.io'; // Replace with your Freshsales domain
-const API_TOKEN = 'sfg999666t673t7t82'; // Replace with your actual Freshsales API token
+const FRESHSALES_DOMAIN = 'fwlabscrm.myfreshworks.com/crm/sales'; // Replace with your Freshsales domain
+const API_TOKEN = 'QPDW5whjK9BWBzWYc7opbA'; // Replace with your actual Freshsales API token
 
 app.post('/create-contact', (req, res) => {
     const { first_name, last_name, email } = req.body;
@@ -14,7 +14,8 @@ app.post('/create-contact', (req, res) => {
         contact: {
             first_name: first_name,
             last_name: last_name,
-            mobile_number: mobile_number
+            email: email,
+            lifecycle_stage_id  :403017878192
         }
     };
 
